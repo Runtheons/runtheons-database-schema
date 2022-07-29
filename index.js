e77a15db33aafc7e782614c5f43f525faf231395
Zexal0807 <gallinar00@gmail.com>
2022-07-29 13:22:29 +0200

    Add idea of package use

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-const associations = require("./models/associations");
+const { Sequelize } = require("sequelize");
 
-module.exports = associations;
+const loadModels = (sequelize) => {
+	const Log = {};
+	const User = require("./models/User")(sequelize);
+	// const User = require("./models/User")(sequelize);
+	// const User = require("./models/User")(sequelize);
+	// const User = require("./models/User")(sequelize);
+
+	return {
+		Log: Log,
+		User: User,
+	};
+};
+
+const loadRelations = (models) => {
+	require("./relations/User")(models);
+};
+
+const loadHooks = (models) => {
+	require("./hooks/User")(models);
+};
+
+let defaultConfig = require("./tests/config.json");
+
+module.exports = async(config = defaultConfig.db) => {
+	const sequelize = new Sequelize(config);
+
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+
+	const models = loadModels(sequelize);
+	loadRelations(models);
+	loadHooks(models);
+
+	return {...models, sequelize };
+};
