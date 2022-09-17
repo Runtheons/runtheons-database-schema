2ca97043c54a5b0af5e0458c1d867e71bf354cd2
Zexal0807 <gallinar00@gmail.com>
2022-09-17 13:45:43 +0200

    indent

--- migrations/20220916195630-TMP-insert-loginmethods.js
+++ migrations/20220916195630-TMP-insert-loginmethods.js
@@ -4,14 +4,14 @@ module.exports = {
 	up: async(queryInterface, Sequelize) => {
 		await queryInterface.sequelize
 			.query(`INSERT INTO loginmethods(idUser, type, email, password, dateCreation, lastUpdate)
-      SELECT 
-          U.id AS idUser, 
-          "CLASSIC" AS type,
-          U.email AS email,
-          U.password AS password,
-          U.dateCreation AS dateCreation,
-          U.lastUpdate AS dateCreation
-      FROM users AS U`);
+			SELECT 
+				U.id AS idUser, 
+				"CLASSIC" AS type,
+				U.email AS email,
+				U.password AS password,
+				U.dateCreation AS dateCreation,
+				U.lastUpdate AS dateCreation
+			FROM users AS U`);
 	},
 
 	down: async(queryInterface, Sequelize) => {},
