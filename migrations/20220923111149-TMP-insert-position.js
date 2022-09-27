cfc27ce407b7fb1b1faa33524a5da8106038d6cc
Zexal0807 <gallinar00@gmail.com>
2022-09-27 12:30:22 +0200

    Insert position

---
+++ migrations/20220923111149-TMP-insert-position.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+	up: async(queryInterface, Sequelize) => {
+		try {
+			let [positions, metadata] = await queryInterface.sequelize.query(`SELECT id, position FROM OLD_users WHERE position IS NOT NULL`);
+			for (let i = 0; i < positions.length; i++) {
+				let position = positions[i];
+
+				if (
+					position.position.coordinates[0] < position.position.coordinates[1]
+				) {
+					position.position.coordinates = [position.position.coordinates[1], position.position.coordinates[0]];
+				}
+
+				if (position.position.coordinates[0] == position.position.coordinates[1]) {
+					continue;
+				}
+
+				let [idPosition, metadata] = await queryInterface.sequelize.query(`INSERT INTO positions(latitude, longitude) VALUES (${position.position.coordinates[0]},${position.position.coordinates[1]})`);
+
+				let result = await queryInterface.sequelize.query(`UPDATE users SET idPosition = ${idPosition} WHERE idUser=${position.id}`);
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	},
+
+	down: async(queryInterface, Sequelize) => {
+		await queryInterface.sequelize.query(`UPDATE users SET idPosition = NULL WHERE idPosition IS NOT NULL`);
+		await queryInterface.sequelize.query(`DELETE FROM positions`);
+	}
+};
