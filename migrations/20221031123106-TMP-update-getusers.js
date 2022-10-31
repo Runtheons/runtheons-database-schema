347bbc1c007aa68c9e0c75eed79846cca2f4ce32
Zexal0807 <gallinar00@gmail.com>
2022-10-31 13:40:07 +0100

    Fix getusers

--- migrations/20221031123106-TMP-update-getusers.js
+++ migrations/20221031123106-TMP-update-getusers.js
@@ -1,4 +1,10 @@
-SELECT
+'use strict';
+
+module.exports = {
+	up: async(queryInterface, Sequelize) => {
+		try {
+			await queryInterface.sequelize.query(`ALTER VIEW getusers AS
+			SELECT
     users.idUser AS idUser,
     users.idUser AS id,
     loginmethods.email AS email,
@@ -62,4 +68,61 @@ LEFT JOIN (
     FROM usersspecialities
     INNER JOIN specialities ON specialities.idSpeciality = usersspecialities.idSpeciality
     GROUP BY idUser
-) AS subspecialities ON subspecialities.idUser = users.idUser
+) AS subspecialities ON subspecialities.idUser = users.idUser`);
+		} catch (e) {
+			console.log(e);
+		}
+	},
+
+	down: async(queryInterface, Sequelize) => {
+		try {
+			await queryInterface.sequelize.query(`ALTER VIEW getusers AS
+			SELECT
+				users.id AS idUser,
+				users.id AS id,
+				users.email AS email,
+				users.password AS password,
+				users.status AS status,
+				users.idCustomer AS idCustomer,
+				users.userType AS userType,
+				users.type AS type,
+				users.position AS position,
+				users.address_name AS address_name,
+				users.photo AS photo,
+				users.cover AS cover,
+				users.sex AS sex,
+				users.bio AS bio,
+				users.dateBirth AS dateBirth,
+				users.phone AS phone,
+				users.dateCreation AS dateCreation,
+				users.lastUpdate AS lastUpdate,
+				athletes.name AS aname,
+				athletes.surname AS asurname,
+				agoals.idGoal AS agoalid,
+				agoals.description AS agoaldescription,
+				sports.idSport AS sportid,
+				sports.description AS sportdescription,
+				professionists.name AS pname,
+				professionists.surname AS psurname,
+				professionists.motto AS motto,
+				professionists.stripeAccount AS stripeAccount,
+				professionists.curriculum AS curriculum,
+				professionists.workOnline AS workOnline,
+				professionists.radius AS radius,
+				pgoals.idGoal AS pgoalid,
+				pgoals.description AS pgoaldescription,
+				specialities.idSpeciality AS specialityid,
+				specialities.description AS specialitydescription
+			FROM users
+			LEFT JOIN athletes ON users.id = athletes.idUser AND users.type = "ATHLETE"
+			LEFT JOIN goals AS agoals ON athletes.idGoal = agoals.idGoal AND agoals.type = "ATHLETE"
+			LEFT JOIN sports ON athletes.idSport = sports.idSport
+
+			LEFT JOIN professionists ON users.id = professionists.idUser AND users.type = "PROFESSIONIST"
+			LEFT JOIN goals AS pgoals ON professionists.idGoal = pgoals.idGoal AND pgoals.type = "PROFESSIONIST"
+			LEFT JOIN specialities ON professionists.idSpeciality = specialities.idSpeciality`);
+		} catch (e) {
+			console.log(e);
+		}
+	}
+};
