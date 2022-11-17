acf802d65c03ee59c8ae8b98e53b572cb00b51cb
Zexal0807 <gallinar00@gmail.com>
2022-11-17 10:52:34 +0100

    Rename old table

--- migrations/20221114131446-TMP-rename-table.js
+++ migrations/20221114131446-TMP-rename-table.js
@@ -3,14 +3,34 @@
 module.exports = {
 	up: async(queryInterface, Sequelize) => {
 		try {
-			await queryInterface.renameTable("sporttarget", "OLD_sporttarget");
+			await queryInterface.renameTable("professionists", "OLD_professionists");
+		} catch (e) {
+			console.log(e);
+		}
+		try {
+			await queryInterface.renameTable("athletes", "OLD_athletes");
+		} catch (e) {
+			console.log(e);
+		}
+		try {
+			await queryInterface.renameTable("administrators", "OLD_administrators");
 		} catch (e) {
 			console.log(e);
 		}
 	},
 	down: async(queryInterface, Sequelize) => {
 		try {
-			await queryInterface.renameTable("OLD_sporttarget", "sporttarget");
+			await queryInterface.renameTable("OLD_professionists", "professionists");
+		} catch (e) {
+			console.log(e);
+		}
+		try {
+			await queryInterface.renameTable("OLD_athletes", "athletes");
+		} catch (e) {
+			console.log(e);
+		}
+		try {
+			await queryInterface.renameTable("OLD_administrators", "administrators");
 		} catch (e) {
 			console.log(e);
 		}
